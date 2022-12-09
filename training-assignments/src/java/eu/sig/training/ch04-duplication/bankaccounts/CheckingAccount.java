09bb85a37a426ea69cd9548fcabbe46c378d0f4b
JellevanE <73659687+JellevanE@users.noreply.github.com>
2022-12-09 14:20:54 +0100

    Update checking account 2
    to improve duplication / maintainability

--- training-assignments/src/java/eu/sig/training/ch04-duplication/bankaccounts/CheckingAccount.java
+++ training-assignments/src/java/eu/sig/training/ch04-duplication/bankaccounts/CheckingAccount.java
@@ -1,36 +1,19 @@
-package eu.sig.training.ch04.v2;
+package eu.sig.training.ch04.v3;
 
 import eu.sig.training.ch04.BusinessException;
 import eu.sig.training.ch04.Money;
 
 // tag::CheckingAccount[]
-public class CheckingAccount {
+public class CheckingAccount extends Account {
     private int transferLimit = 100;
 
+    @Override
     public Transfer makeTransfer(String counterAccount, Money amount)
         throws BusinessException {
-        // 1. Check withdrawal limit:
         if (amount.greaterThan(this.transferLimit)) {
             throw new BusinessException("Limit exceeded!");
         }
-        if (Accounts.isValid(counterAccount)) { // <1>
-            // 2. Look up counter account and make transfer object:
-            CheckingAccount acct = Accounts.findAcctByNumber(counterAccount);
-            Transfer result = new Transfer(this, acct, amount); // <2>
-            return result;
-        } else {
-            throw new BusinessException("Invalid account number!");
-        }
+        return super.makeTransfer(counterAccount, amount);
     }
-
-    public void addInterest() {
-        Money interest = balance.multiply(INTEREST_PERCENTAGE);
-        if (interest.greaterThan(0)) {
-            balance.add(interest);
-        } else {
-            balance.substract(interest);
-        }
-    }
-
 }
 // end::CheckingAccount[]
